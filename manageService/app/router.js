db36d3697791ce7d382dd0adb3f2a19dd6c4f80d
huangwensong <huangwensong@58.com>
2023-10-16 10:27:58 +0800

    feat: 更新

--- manageService/app/router.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-    const { router, controller } = app;
-    console.log(router);
-    router.get('/', controller.home.index);
-    router.get('/news', controller.news.list);
-}
